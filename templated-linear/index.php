4415c786284f794338339efdf4db628570386f2c
LuChungYing <35101191+LuChungYing@users.noreply.github.com>
2020-05-25 14:28:55 +0800

    Add files via upload

---
+++ templated-linear/index.php
@@ -0,0 +1,68 @@
+<?php
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : EntryWay
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140124
+
+-->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1></span><a href="#">老照片的記憶</a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="%" accesskey="1" title="">人物介紹</a></li>
+				<li><a href="$" accesskey="2" title="">背包</a></li>
+				<li><a href="@" accesskey="3" title="">支線任務</a></li>
+				<li><a href="*" accesskey="4" title="">地圖</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div id="header-featured">
+	<div id="banner" class="container"> </div>
+</div>
+<div id="wel">
+	<div class="container">
+		<form method="POST">
+		<p>你的答案</p> <input name="name" type="text"><br>
+		<br>
+		<input type="submit" name="submit" value="確認"><br>
+		<br>
+		</form>
+		<p>故事
+		</p>
+	</div>
+</div>
+
+
+<div id="copyright" class="container">
+	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+</body>
+</html>
