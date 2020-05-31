de849112ea5154e4f46d5e0b384a66d846fe9a7e
tl32rodan <brian90218774@gmail.com>
2020-05-31 18:00:42 +0800

    Complete step2y.php

--- enter_game/step2y.php
+++ enter_game/step2y.php
@@ -4,11 +4,6 @@
 		　alert("把代碼輸入到google map");
 		}
 </script>
-<script>
-function express(){
-location.href="step1n.php";
-}
-</script> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--
 Design by TEMPLATED
@@ -60,51 +55,9 @@ Released   : 20140124
 	<div class="container1" style = "text-align: center" >
 <wel>
 <?php
-    echo"
-    <div class=\"container\" style = \"margin: 0 500 20 20\" >
-    <span id=\"Check_Txt\" style=\"color:red\">倒數計時：
-        <span id=\"Check_i\"> 0分0秒
-        </span>
-        </span>
-    </div>
-<script type=\"text/javascript\">
-    var SetMinute = 0;
-    function Check_Time() {
-        SetMinute += 1;
-        var Check_i = document.getElementById(\"Check_i\");
-
-        var Cal_Minute = Math.floor(Math.floor(SetMinute % 3600) / 60);
-        var Cal_Second = SetMinute % 60;
-        if( Cal_Minute > 5 ){
-            express();
-            return;
-        }
-        Check_i.innerHTML =  + Cal_Minute + \"分\" + Cal_Second + \"秒\";
-    }
-    var mm = window.setInterval(\"Check_Time()\", 1000);
-</script>";
     echo "<html><body>";
     echo "<div id=\"wel2\">
-    <div class=\"container2\">
-阿嬤是成人，我雖然國小，但已經有145公分了。這樣我們兩個的票錢總共多少呢？
-</div></div>
-"
-    ;
-echo "
-<wel>
-<div class=\"container\" style = \"margin: 0 30 20 20\" >
-    <form method=\"post\">
-　      你算出來了嗎: <input type=\"test\" name=\"answer\">
-        <input type=\"submit\" name=\"submit\" value=\"submit\">
-    </form>
-
-</div>
-</div>";
-if (isset($_POST["submit"])){
-    if ($_POST["answer"] == "80"){
-    echo "<html><body>";
-    echo "<div id=\"wel2\">
-    <div class=\"container2\" style = \"margin: 10 300 20 -530; font-size: 1.2em;text-align: left\">
+    <div class=\"container2\" style = \"margin: 10 300 20 30; font-size: 1.2em;text-align: left\">
 小時候上了船，阿嬤總會帶我站在船的側邊，感受海風的吹拂，還有偶爾濺起的浪花。雖然很舒服，阿嬤還是會說：「要抓好護欄喔，不小心會掉進海裡的。」<br><br>年輕的阿嬤對落海這件事有著不好的回憶，據說那時，有艘渡輪因為船艙破裂而翻船，船上載滿要到前鎮加工區上班的員工，無一倖免全部罹難。幸好阿嬤不在那艘船上。<br><br>
 
 「那時候中壇元帥然告訴我不要上船，我才沒有上去。」阿嬤從那件事之後，就成了中壇元帥很虔誠的信徒，而這又是另一個故事了。<br><br>
@@ -113,47 +66,15 @@ if (isset($_POST["submit"])){
 
 </div></div>
 ";
-            echo "<div id=\"header2-featured\" style = \"margin: 120 300 100 -230;\; background-size: 750px 700px; width: 450px;\">
+            echo "<div id=\"header2-featured\" style = \"margin: 300 300 50 300;\; background-size: 750px 700px; width: 450px;\">
                          <class=\"container2\" style = \"margin: 0 100 100 0;\" > 
                   </div>";
-        echo "</p>                                                                                                                                             
-        <div class=\"container1\" style= \"margin: -100 300 20 -600  \">
-                    <a onClick=\"ShowMeDate()\" class=\"button\">提示</a> 
+        echo "                                                                                                                                  
+        <div class=\"container1\" style= \"margin: -50 150 20 -100  \">
                     <a href=\"step3.php\" class=\"button\">下一關!</a>
         </div>
         </div>
 ";
         echo "</body></html>";
-    }
-    else{
-        echo "<html><body>";
-    echo "
-    <div id=\"wel2\">
-        <div class=\"container2\" style = \"margin: 0 300 20 -530\" >
-            阿嬤是成人，我雖然國小，但已經有145公分了。這樣我們兩個的票錢總共多少呢？<br><br> 
-        </div>
-
-        <div class=\"container2\" style = \"font-size:4em;margin: 0 300 20 -530\" >
-            輸入錯誤！      
-        </div>
-    </div>
-";
-    }
-}
     ?>
-        </p>
-        <div class="container1" style= "margin: 0 300 0 -600  ">
-                    <a onClick="ShowMeDate()" class="button">提示</a> 
-        </div>
-        </div>
 
-	</div>
-</div>
-
-
-
-<div id="copyright" class="container">
-	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
-</div>
-</body>
-</html>
