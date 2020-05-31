9a1ef4d4827cd8f2fe7b92c11c0c8b8e7306035d
tl32rodan <brian90218774@gmail.com>
2020-05-31 20:50:29 +0800

    Complete step?n.php

--- enter_game/step6n.php
+++ enter_game/step6n.php
@@ -60,31 +60,11 @@ Released   : 20140124
 	<div class="container1" style = "text-align: center" >
 <wel>
 <?php
-    echo"
-    <div class=\"container\" style = \"margin: 0 500 20 20\" >
-    <span id=\"Check_Txt\" style=\"color:red\">倒數計時：
-        <span id=\"Check_i\"> 0分0秒
-        </span>
-        </span>
-    </div>
-<script type=\"text/javascript\">
-    var SetMinute = 0;
-    function Check_Time() {
-        SetMinute += 1;
-        var Check_i = document.getElementById(\"Check_i\");
-
-        var Cal_Minute = Math.floor(Math.floor(SetMinute % 3600) / 60);
-        var Cal_Second = SetMinute % 60;
-        if( Cal_Minute > 5 ){
-            express();
-            return;
-        }
-        Check_i.innerHTML =  + Cal_Minute + \"分\" + Cal_Second + \"秒\";
-    }
-    var mm = window.setInterval(\"Check_Time()\", 1000);
-</script>";
     echo "<html><body>";
     echo "<div id=\"wel2\">
+    <div class=\"container2\" style=\"color:red\" >
+   !!! 時間已經到了喔 你們錯失了拿到提示的大好機會 請進快解出答案!!!<br><br>
+    </div>
     <div class=\"container2\">
     找到圖片所對應的注音，並解析出該項物品為何？<br><br>
    (神秘小提示: 七月三十號，該項賽事將會重啟)
@@ -102,38 +82,13 @@ echo "
 </div>";
 if (isset($_POST["submit"])){
     if ($_POST["answer"] == "籃球"){
-    echo "<html><body>";
-    echo "<div id=\"wel2\">
-    <div class=\"container2\" style = \"margin: 10 300 20 -530; font-size: 1.2em;text-align: left\">
-原來我那時候這麼想要籃球。後來我當然被阿嬤修理了一頓，想要的籃球也沒買到，衣服還髒得要命。真的是得不償失。<br><br>
-
-「阿嬤，為什麼你剛剛說我是泥鰍啊？」回家的路上，我牽著阿媽的手問。<br><br>
-
-「因為以前這裡是一個大水池啊，那時候前鎮河還有一條分支流進來這邊喔。」<br><br>
-
-「哇，那不就是一個超大游泳池了嗎？好酷喔～阿嬤再多說一點以前的故事給我聽～」<br><br>
-
-我想起那天和阿嬤追逐、挨阿嬤罵、邊說故事邊走回家的情景，顧不得旁邊還有一堆唱著卡拉ＯＫ的阿姨叔叔們，突然就哭了起來，而且越哭越大聲。<br><br>
-
-「少年仔，你是怎麼了？怎麼哭成這樣？」<br><br>
-
-「我只是很想念阿嬤，想念那個以前健健康康，還會生氣罵人的阿嬤……」我哭得滿臉鼻涕眼淚，阿姨叔叔們只好帶我到旁邊的興仁國中稍微洗洗臉。
-
-
-</div></div>
-";
-            echo "<div id=\"headerball-featured\" style = \"margin: 300 300 100 -230;\; background-size: 750px 700px; width: 450px;\">
-                         <class=\"container2\" style = \"margin: 0 100 100 0;\" > 
-                  </div>";
-        echo "</p>                                                                                                                                             
-        <div class=\"container1\" style= \"margin: -100 300 20 -600  \">
-                    <a onClick=\"ShowMeDate()\" class=\"button\">提示</a> 
-                    <a href=\"step7.php\" class=\"button\">下一關!</a>
-        </div>
-        </div>
-";
-        echo "</body></html>";
-    }
+        $url  =  "step6y.php" ;
+		echo " <script language = 'javascript'
+			type = 'text/javascript'> ";
+		echo " window.location.href = '$url' ";
+		echo " </script> ";
+	 
+ }
     else{
             echo "</p> <div class=\"wel2\"><div class=\"container2\" style = \"font-size:4em;margin: 0 300 20 -530\" >輸入錯誤！</div></div>";
     }
