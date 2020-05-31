204e54d58d025c34f2ff8525ae224477d9cd2539
tl32rodan <brian90218774@gmail.com>
2020-05-31 19:35:21 +0800

    Complete step3y.php

--- enter_game/step3y.php
+++ enter_game/step3y.php
@@ -4,11 +4,6 @@
 		　alert("把代碼輸入到google map");
 		}
 </script>
-<script>
-function express(){
-location.href="step1n.php";
-}
-</script> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--
 Design by TEMPLATED
@@ -59,55 +54,12 @@ Released   : 20140124
 <div id="wel">
 	<div class="container1" style = "text-align: center" >
 <wel>
-<?php
-    echo"
-    <div class=\"container\" style = \"margin: 0 500 20 20\" >
-    <span id=\"Check_Txt\" style=\"color:red\">倒數計時：
-        <span id=\"Check_i\"> 0分0秒
-        </span>
-        </span>
-    </div>
-<script type=\"text/javascript\">
-    var SetMinute = 0;
-    function Check_Time() {
-        SetMinute += 1;
-        var Check_i = document.getElementById(\"Check_i\");
-
-        var Cal_Minute = Math.floor(Math.floor(SetMinute % 3600) / 60);
-        var Cal_Second = SetMinute % 60;
-        if( Cal_Minute > 5 ){
-            express();
-            return;
-        }
-        Check_i.innerHTML =  + Cal_Minute + \"分\" + Cal_Second + \"秒\";
-    }
-    var mm = window.setInterval(\"Check_Time()\", 1000);
-</script>";
-    echo "<html><body>";
-    echo "<div id=\"wel2\">
-    <div class=\"container2\" >
-    這一關還沒有想到題目，請在下方輸入\"俊穎好帥\"以過關:)
-</div></div>
-"
-    ;
-echo "
-<wel>
-<div class=\"container\" style = \"margin: 0 300 20 20\" >
-    <form method=\"post\">
-　  這裡輸入：   <input type=\"test\" name=\"answer\">
-        <input type=\"submit\" name=\"submit\" value=\"submit\">
-    </form>
-
-</div>
-</div>";
-if (isset($_POST["submit"])){
-    if ($_POST["answer"] == "俊穎好帥"){
-    echo "<html><body>";
-    echo "<div id=\"wel2\">
-    <div class=\"container2\" style = \"margin: 10 300 20 -530; font-size: 1.2em;text-align: left\">
+    <html><body>
+    <div id="wel2">
+    <div class="container2" style = "margin: 10 300 20 30; font-size: 1.2em;text-align: left">
 「這裡主要供奉的是關聖帝君，又稱文衡聖帝。樓上佛祖殿則是供奉觀音佛祖。」一旁的阿姨跟我說。<br><br>
 
-「你是管區嬤的孫子吧？她最近還好嗎？」\"管區嬤\"是阿嬤上了年紀後，大家對她的暱稱。因為她就像個管區，前鎮的大小事幾乎都仰賴她的協助。<br><br>
+「你是管區嬤的孫子吧？她最近還好嗎？」"管區嬤"是阿嬤上了年紀後，大家對她的暱稱。因為她就像個管區，前鎮的大小事幾乎都仰賴她的協助。<br><br>
 
 「妳是以前常常跟他吵架的主委！」我猛地認了出來，原來她還在這邊服務。「妳不是和她關係不好嗎？怎麼突然關心起她了？」<br><br>
 
@@ -119,43 +71,23 @@ if (isset($_POST["submit"])){
 
 我收下了主委阿姨的叮嚀，趕緊將下一個代號輸入google地圖。<br><br>
 
+</div>
+
+
+<div class="container2" style = "margin: -100 300 20 30; font-size: 1.2em;text-align: center">
 </div></div>
-";
-            echo "<div id=\"headercharm-featured\" style = \"margin: 450 300 100 -230;\; background-size: 750px 700px; width: 450px;\">
-                         <class=\"container2\" style = \"margin: 0 100 100 0;\" > 
-                  </div>";
-        echo "</p>                                                                                                                                             
-        <div class=\"container1\" style= \"margin: -100 300 20 -600  \">
-                    <a onClick=\"ShowMeDate()\" class=\"button\">提示</a> 
-                    <a href=\"step4.php\" class=\"button\">下一關!</a>
-        </div>
-        </div>
-";
-        echo "</body></html>";
-    }
-    else{
-        echo "<html><body>";
-    echo "<div id=\"wel2\">
-            <div class=\"container2\" style = \"font-size:4em;margin: 0 300 20 -530\" >輸入錯誤！
-        </div>
-</div>
-";
-    }
-}
-    ?>
-        </p>
-        <div class="container1" style= "margin: 0 300 0 -600  ">
-                    <a onClick="ShowMeDate()" class="button">提示</a> 
-        </div>
+
+<div id="headercharm-featured" style = "margin: 600 300 100 230;\; background-size: 750px 700px; width: 450px;">
+                         <class="container2" style = "margin: 0 100 100 0;" > 
+                  </div>
+        <div class="container1" style= "margin: -50 150 20 -100 ">
+                    <a href="step4.php" class="button">下一關!</a>
         </div>
 
-	</div>
 </div>
 
 
-
-<div id="copyright" class="container">
-	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
 </div>
 </body>
 </html>
+
