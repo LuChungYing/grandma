968885400310416a34ab8406f8f384b717a3f1fc
tl32rodan <brian90218774@gmail.com>
2020-06-08 16:24:45 +0800

    Remove menu in before.php & correct answer of 輪渡站

--- enter_game/step2n.php
+++ enter_game/step2n.php
@@ -81,7 +81,7 @@ echo "
 </div>
 </div>";
 if (isset($_POST["submit"])){
-    if ($_POST["answer"] == "80"){
+    if ($_POST["answer"] == "55"){
         $url  =  "step2y.php" ;
 		echo " <script language = 'javascript'
 			type = 'text/javascript'> ";
